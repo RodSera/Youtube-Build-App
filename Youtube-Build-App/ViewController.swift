71bc6f85d7d4407a70a6fea5b1c4ba98ac7e7995
Rod Sera <seraphinro001@gmail.com>
2020-05-27 18:21:21 -0400

    initial xcode project creation

---
+++ Youtube-Build-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube-Build-App
+//
+//  Created by Rodriguez Seraphin on 5/27/20.
+//  Copyright © 2020 Rodriguez Seraphin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
