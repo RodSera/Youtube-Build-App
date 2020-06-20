278c2f40a20a8b582cd19ed60e9206d5203a24c9
Rod Sera <seraphinro001@gmail.com>
2020-06-20 17:24:40 -0400

    modified git ignore to stop tracking Constants.swift, added ConstantTemplate.swift

---
+++ Youtube-Build-App/ConstantsTemplate.swift
@@ -0,0 +1,20 @@
+//
+//  ConstantsTemplate.swift
+//  Youtube-Build-App
+//
+//  Created by Rodriguez Seraphin on 6/20/20.
+//  Copyright © 2020 Rodriguez Seraphin. All rights reserved.
+//
+
+/* add your own key and uncomment the code */
+
+import Foundation
+
+/*
+struct Constants {
+
+static var API_KEY = "[]"
+static var PLAYLIST_ID = "PLlSchMYV_ETsTCLl6sGplEZGSCMcIms-Q"
+static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+
+*/
